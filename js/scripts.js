76acb99dc8149b1b05d37f4929a179fd6c82769d
srhhttnr <shuettner12@gmail.com>
2024-01-30 11:53:06 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
 function newItem() {
 
 // adding a new item to the list of items
-let li = $('<li></li>');
-let inputValue = $('#input').val();
+let li = $("<li></li>");
+let inputValue = $("#input").val();
 li.append(inputValue);
 
-if (inputValue === '') {
-  alert('You must write something!');
+if (inputValue === "") {
+  alert("You must write something!");
 } else {
-  let list = $('#list');
-  list.append(li);
+  $("#list").append(li);
 }
 
 // crossing out an item from the list of items
-li.on('dblclick', function () {
-  li.toggleClass('strike');
+li.on("dblclick", function () {
+  li.toggleClass("strike");
 });
 
 // adding the delete button "X": 
-let crossOutButton = ('<crossOutButton></crossOutButton>');
-$('crossOutButton').append('X');
+let crossOutButton = ("<crossOutButton></crossOutButton>");
+$("crossOutButton").append("X");
 li.append(crossOutButton);
 
 // adding class 'delete' to list items when "X" button is clicked
-crossOutButton.on('click', function () {
-  li.addClass('delete');
+crossOutButton.on("click", function () {
+  li.addClass("delete");
 });
 
 // making the items reorderable by dragging and dropping them 
-$('#list').sortable();
+$("#list").sortable();
 
 // making inputValue blank after the user adds the last list item
-$('#input').val('');
+$("#input").val("");
 
 }
