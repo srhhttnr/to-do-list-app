24af07b8ac4c5f8fc977ec6f027ffb84ce3fcf8f
srhhttnr <shuettner12@gmail.com>
2024-02-06 13:10:51 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,34 @@
 function newItem() {
 
-// adding a new item to the list of items
-let li = $("<li></li>");
-let inputValue = $("#input").val();
-li.append(inputValue);
+  // adding a new item to the list of items
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
 
-if (inputValue === "") {
-  alert("You must write something!");
-} else {
-  $("#list").append(li);
-}
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
 
-// crossing out an item from the list of items
-li.on("dblclick", function () {
-  li.toggleClass("strike");
-});
+  // crossing out an item from the list of items
+  li.on("dblclick", function () {
+    li.toggleClass("strike");
+  });
 
-// adding the delete button "X": 
-let crossOutButton = ("<crossOutButton></crossOutButton>");
-$("crossOutButton").append("X");
-li.append(crossOutButton);
+  // adding the delete button "X": 
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  $(crossOutButton).append("X");
+  li.append(crossOutButton);
 
-// adding class 'delete' to list items when "X" button is clicked
-crossOutButton.on("click", function () {
-  li.addClass("delete");
-});
+  // adding class 'delete' to list items when "X" button is clicked
+  crossOutButton.on("click", function () {
+   li.addClass("delete");
+  });
 
-// making the items reorderable by dragging and dropping them 
-$("#list").sortable();
+  // making the items reorderable by dragging and dropping them 
+  $("#list").sortable();
 
-// making inputValue blank after the user adds the last list item
-$("#input").val("");
-
+  // making inputValue blank after the user adds the last list item
+  $("#input").val("");
 }
